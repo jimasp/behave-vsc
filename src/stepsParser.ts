2bcefc7a78ba354c7b0a7c06f56ad8238bd5e3ae
Jim Macdonald <jim.macdonald@aug-data.com>
2022-04-06 20:21:14 +0100

    parse multiline steps

--- src/stepsParser.ts
+++ src/stepsParser.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { getContentFromFilesystem } from './helpers';
 
 const stepRe = /^(\s*)(@step|@given|@when|@then|@and)\((.?)("|')(.+)("|')\)(\s*)$/i;
+const startRe = /^(\s*)(@step|@given|@when|@then|@and).+/i;
 
 export class StepDetail {
   constructor(public uri: vscode.Uri, public range: vscode.Range) { }
@@ -24,26 +25,58 @@ export const parseStepsFile = async (uri: vscode.Uri, steps: Steps) => {
       map.delete(key);
   });
 
+
+  let multi = false;
+  let multiText = "";
+  let startLine = 0;
   const lines = content.trim().split('\n');
 
+
   for (let lineNo = 0; lineNo < lines.length; lineNo++) {
-    const line = lines[lineNo].trim();
+
+    let line = lines[lineNo].trim();
 
     if (line === '' || line.startsWith("#")) {
       continue;
     }
 
+    if (line.endsWith("\\"))
+      line.slice(0, -1);
+
+    if (multi) {
+      if (line.startsWith(")")) {
+        multiText = multiText.replaceAll('""', "");
+        multi = false;
+      }
+      else {
+        multiText += line;
+        continue;
+      }
+    }
+
+    if (startRe.exec(line) && line.endsWith("(")) {
+      startLine = lineNo;
+      multi = true;
+      continue;
+    }
+
+    if (multiText)
+      line = "@step(" + multiText + ")";
+    else
+      startLine = lineNo;
+
     const step = stepRe.exec(line);
     if (step) {
-      const range = new vscode.Range(new vscode.Position(lineNo, 0), new vscode.Position(lineNo, step[0].length));
+      multiText = "";
       let stepText = step[5].trim();
       stepText = stepText.replace(/[.*+?^$()|[\]]/g, '\\$&'); // escape any regex chars except for \ { }
       stepText = stepText.replace(/{.*?}/g, '.+');
       const reKey = `^${stepText}$`;
+      const range = new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(lineNo, step[0].length));
       const detail = new StepDetail(uri, range);
       steps.set(reKey, detail); // there can be only one
     }
+
   }
 
 };
-
