b1f216026a77205817bb7676d0f665df89120e3d
Jim Macdonald <jim.macdonald@aug-data.com>
2022-08-26 08:41:58 +0100

    fix pytest debug stop

--- src/extension.ts
+++ src/extension.ts
@@ -75,15 +75,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<TestSu
       vscode.commands.registerTextEditorCommand(`behave-vsc.gotoStep`, gotoStepHandler),
       vscode.commands.registerTextEditorCommand(`behave-vsc.findStepReferences`, findStepReferencesHandler),
       vscode.commands.registerCommand(`behave-vsc.stepReferences.prev`, prevStepReferenceHandler),
-      vscode.commands.registerCommand(`behave-vsc.stepReferences.next`, nextStepReferenceHandler),
-
-      // this isn't the best mechanism for detecting debug stop, but its the best we can do for now.
-      // it should work just fine as long as the function "cancelTestRun" returns fast and cannot fail.
-      // (since changes to debugpy, its currently impossible using a debug tracker to distinguish between a user clicking 
-      // a debug stop or a normal automatic debug session termination on windows.
-      // on the plus side this reacts much faster than using a debug tracker and so it gives a much better user experience)
-      // see: https://github.com/microsoft/vscode/issues/145159
-      vscode.commands.registerCommand("workbench.action.debug.stop", () => cancelTestRun("debug stop clicked"))
+      vscode.commands.registerCommand(`behave-vsc.stepReferences.next`, nextStepReferenceHandler)
     );
 
     const removeTempDirectoryCancelSource = new vscode.CancellationTokenSource();
