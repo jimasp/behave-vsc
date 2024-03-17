9d8025e8ca4d4a9b38c686143aab7e22da4612b0
jimasp <464080+jimasp@users.noreply.github.com>
2024-03-17 15:02:50 +0000

    run profiles wip

--- src/_tests/integration/multiroot suite/index.ts
+++ src/_tests/integration/multiroot suite/index.ts
@@ -4,26 +4,37 @@ import { testGlobals } from "../_common/types";
 
 
 
-// MULTIROOT SUITE SPECIFIES ITS OWN TEST FILE GLOBS (AND IGNORES) SUCH THAT
+// HERE (MULTIROOT SUITE) WE SPECIFY THE multi.test.js TEST FILE GLOBS SUCH THAT
 // IT WILL RUN PROJECTS IN PARALLEL (TO SIMULATE runMultiRootProjectsInParallel=True).
 export async function run(): Promise<void[]> {
 
 	testGlobals.multiRootTest = true;
 	vscode.commands.executeCommand("testing.clearTestResults");
 
-	return Promise.all([
-		// we could include all these in a single glob, but it's better to keep them separate 
-		// so we can comment them out individually whenever required for debugging purposes.		
-		runner("../**/simple suite/multi.tests.js"),
-		runner("../**/project A suite/multi.tests.js"),
-		runner("../**/project B suite/multi.tests.js"),
-		runner("../**/run profiles suite/multi.tests.js"),
-		runner("../**/multiple top-level features folders suite/multi.tests.js"),
-		runner("../**/imported steps suite/multi.tests.js"),
-		runner("../**/sibling steps folder 1 suite/multi.tests.js"),
-		runner("../**/sibling steps folder 2 suite/multi.tests.js"),
-		runner("../**/working dir suite/multi.tests.js"),
-		runner("../**/handle bad import suite/multi.tests.js"),
-		runner("../**/use custom runner suite/multi.tests.js"),
-	]);
+
+	// we could include all these in a single glob and use a single runner, but keeping them separate 
+	// means we can comment them out individually as needed whenever required for debugging purposes.			
+	const multis = [
+		"../**/simple suite/multi.tests.js",
+		"../**/project A suite/multi.tests.js",
+		"../**/project B suite/multi.tests.js",
+		"../**/run profiles suite/multi.tests.js",
+		"../**/multiple top-level features folders suite/multi.tests.js",
+		"../**/imported steps suite/multi.tests.js",
+		"../**/sibling steps folder 1 suite/multi.tests.js",
+		"../**/sibling steps folder 2 suite/multi.tests.js",
+		"../**/working dir suite/multi.tests.js",
+		"../**/handle bad import suite/multi.tests.js",
+		"../**/use custom runner suite/multi.tests.js",
+	];
+
+	// because we're creating lots of new runners rather than using one glob, and 
+	// each mocha.run will call _addEventListener for uncaughtException and unhandledRejection, so 
+	// to stop a MaxListenersExceededWarning being logged, we'll increase the max listeners
+	console.log(process.getMaxListeners());
+	process.setMaxListeners(multis.length + 1);
+
+	return Promise.all(multis.map(m => runner(m)));
+
+
 }
